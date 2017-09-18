af08cb2c77dfaf7e601b353549c1b3ff988778c0
Jonathan <jon_mcdevitt@hotmail.com>
2017-09-18 11:24:36 -0300

    Adding in basic constructors and getters/setters, as well as tests for them.

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,2 +1,89 @@
-public class LinkedList {
+import java.util.Collection;
+
+public class LinkedList<T> {
+    private Node<T> head, tail;
+    private int size;
+
+    public LinkedList() {
+        head = tail = null;
+        size = 0;
+    }
+
+    public LinkedList(T data) {
+        head = tail = new Node<>(data);
+        size = 1;
+    }
+
+    public LinkedList(Collection<T> list) {
+        head = tail = null;
+        size = 0;
+        Node<T> curr = null;
+
+        /* Iterate through each item in the list and add it.   */
+        for (T item : list) {
+            if(size==0) {   /* If we are at the beginning, initialize the head. */
+                head = new Node<>(item);
+                curr = head;
+            } else {    /* If we are in the middle or at the end of the list, simply append to the current pointer. */
+                curr.next = new Node<>(item);
+                curr.next.prev = curr;
+                curr = curr.next;
+            }
+            size++;
+        }
+
+        /* Finally, set the tail to be the last item in the list. */
+        tail = curr;
+    }
+
+    public int Size() {
+        return size;
+    }
+
+    public T GetHead() {
+        return head.data;
+    }
+
+    public T GetTail() {
+        return tail.data;
+    }
+
+    private class Node<E> {
+        E data;
+        Node<E> next, prev;
+
+        Node() {
+            data = null;
+            next = prev = null;
+        }
+
+        Node(E data) {
+            this.data = data;
+            next = prev = null;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+
+        public Node<E> getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node<E> prev) {
+            this.prev = prev;
+        }
+    }
 }
--- test/LinkedListTest.java
+++ test/LinkedListTest.java
@@ -1,5 +1,37 @@
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListTest {
+    private LinkedList<String> namesList;
 
+    @BeforeEach
+    void setUp() {
+        namesList = new LinkedList<>();
+        assertNotNull(namesList);
+    }
+
+    @Test
+    void ConstructWithSingleData() {
+        namesList = new LinkedList<>("Danny");
+        assertNotNull(namesList);
+    }
+
+    @Test
+    void ConstructWithCollection() {
+        ArrayList<String> testNames = new ArrayList<>();
+        testNames.add("Dan");
+        testNames.add("Grant");
+        testNames.add("Ruji");
+        testNames.add("Matt");
+        testNames.add("Devon");
+        testNames.add("Ana");
+
+        namesList = new LinkedList<>(testNames);
+        assertNotNull(namesList);
+        assertTrue(namesList.Size() == testNames.size());
+        assertTrue(namesList.GetHead().equals(namesList.GetTail()));
+    }
 }
